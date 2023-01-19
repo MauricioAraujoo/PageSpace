3871101738d5e82304f199088b6b8d89069df5db
Mauricio Araujo <malaraujo@uea.edu.br>
2023-01-19 09:39:17 -0400

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,22 @@
-import {Hdiv, H1} from './HeaderStyles'
+import { Hdiv } from "./HeaderStyles";
+import logo from "../../../Public/assets/FRONTEIRA-Logo.png"
 
 export const Header = () => {
-    return (
-        <Hdiv>
-            <H1>askjdgakjgdkjagd</H1>
-        </Hdiv>
-    )
-}
+  return (
+    <Hdiv>
+      <section>
+        <nav>
+            <ul className="actions1">
+              <a><span><img src={logo} alt="FRONTEIRA-Logo.png" /></span></a>
+              <li>MEMBERS</li>
+              <li>ORGANIZAÇÂO</li>
+              <li>DIVISIONS</li>
+              <li>DISCORD</li>
+              <li>SUPPORT</li>
+              <li>ACCOUNT</li>
+            </ul>
+        </nav>
+      </section>
+    </Hdiv>
+  );
+};
--- src/components/header/HeaderStyles.js
+++ src/components/header/HeaderStyles.js
@@ -1,16 +1,33 @@
 import styled from "styled-components";
 
-export const H1 = styled.h1`
-color: #fff ;
-
-`
 export const Hdiv = styled.div`
-    background-color: #000;
-    opacity: 0.5;
     height: 100px;
     width: 100%;
-   
     
+
+    nav{    
+        align-items: center;
+       
+        
+        background-color: rgba(13,46,66,.5);
+    }
+    img{
+        width: 100px;
+    }
+    li{
+        list-style: none;
+        cursor: pointer;
+        color: #7e7e7e;;
+    }
+    li:hover{
+        color: #FFF;
+    }
+    .actions1{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+    }
+
 `
 
 
